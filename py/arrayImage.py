6befa8bd7d5152e6097a88586a8d6199b57aed2c
Ashley Crouch <ashley.crouch@rocketmail.com>
2018-05-25 14:51:58 -0400

    added py libraries

---
+++ py/arrayImage.py
@@ -0,0 +1,13 @@
+from numpy import *
+import skimage.io
+# from PIL import Image
+
+filename = 'C:/Users/Ashley/Documents/GitHub/image-rgb-in-3D/oranges.jpg'
+img = skimage.io.imread( filename )
+# width, height = img.shape
+# print('width: ' , width, 'height: ', height)
+
+# # meshgrid( width, height )
+# imageArray = append( meshgrid( width, height ), img )
+
+
