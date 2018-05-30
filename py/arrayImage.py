9306f3ef22048d7dc9bfe28bc9af449668fb9a0c
Ashley Crouch <ashley.crouch@rocketmail.com>
2018-05-30 19:48:17 -0400

    moved some things around, changed gradientbox to a rectangle

--- py/arrayImage.py
+++ py/arrayImage.py
@@ -1,7 +1,8 @@
-# from numpy import *
 import numpy as np
-# np.set_printoptions( threshold=np.nan )
 import skimage.io
+from matplotlib import pylab
+from mpl_toolkits import mplot3d
+# from ransac import *
 
 filename = 'C:/Users/Ashley/Documents/GitHub/image-rgb-in-3D/gradientbox.png'
 img = skimage.io.imread( filename )
@@ -18,22 +19,21 @@ heightAr = list(range(0, height))
 # print(heightAr)
 widthAr = list(range(0, width))
 # print(widthAr)
-# print(np.cross(heightAr, widthAr))
 meshGrid = np.meshgrid(widthAr, heightAr)
 imageArray = np.concatenate((meshGrid[0][:, :, None], meshGrid[1][:, :, None], img), axis = 2)
-print(imageArray[0, 2])
-# print(imageArray.shape)
+# print(imageArray[0, 2])
+print(imageArray.shape)
 
 # reshaped is the input for the plane-fitting
 reshaped = imageArray.reshape(width * height, -1)
 print(reshaped)
 print(reshaped[0])
 
-def printImageArray(imageArray):
-    length = len(imageArray) / 5
-    n = 0
-    for i in range(0, int(length)):
-        print('Pixel #', i, imageArray[n:n+5])
-        n += 5
+# def printImageArray(imageArray):
+#     length = len(imageArray) / 5
+#     n = 0
+#     for i in range(0, int(length)):
+#         print('Pixel #', i, imageArray[n:n+5])
+#         n += 5
 
 # printImageArray(imageArray)
